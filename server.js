746dcfafcd48f30f5246b061931a473b3cead2a3
Ubuntu <ubuntu@ip-172-31-37-217.ap-south-1.compute.internal>
2022-09-09 11:24:29 +0000

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Guys,123 @Volansys!!');
+  res.send('Hello Guys,345 @Volansys!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
