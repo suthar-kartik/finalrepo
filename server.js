8bb0f0ba8d1a3cb3f6a8a6a78afc2963800b5557
Ubuntu <ubuntu@ip-172-31-37-217.ap-south-1.compute.internal>
2022-09-09 10:27:43 +0000

    lets see changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Guys, @Volansys!!');
+  res.send('Hello Guys,123 @Volansys!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
