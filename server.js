cae774da668e7388a72e0e4d7dc53619bfdd4b87
Ubuntu <ubuntu@ip-172-31-37-217.ap-south-1.compute.internal>
2022-09-09 10:10:14 +0000

    changed the content

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Guys!!');
+  res.send('Hello Guys, Well come to Volansys!!');
 });
 app.listen(PORT);
-console.log(Running on ${PORT});
+console.log(`Running on ${PORT}`);
