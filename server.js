9c0b3e21b2dd85e9bd6328b9bc351ba7462b40b0
suthar-kartik <kartik.suthar@volansys.com>
2022-09-14 08:58:10 +0000

    fsdfsf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = 8000;
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello guys,jkhkhkvolansys!!');
+  res.send('Hello guys,jkvolansys!!');
 });
 app.listen(PORT);
 console.log(`Running on ${PORT}`);
